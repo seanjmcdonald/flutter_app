449e241d1b42d026ccca7316ff882dd94862f692
Sean McDonald <sean.john.mcdonald@gmail.com>
2019-03-14 00:55:46 -0700

    more progress

--- lib/login.dart
+++ lib/login.dart
@@ -41,6 +41,40 @@ class _LoginPage extends State<LoginPage> {
     }
   }
 
+  Widget loadText(){
+
+    return Container(
+      padding: EdgeInsets.all(50),
+      child:Form(
+      key: this._formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            style: TextStyle(color: Colors.white),
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration.collapsed(
+              hintText: "your email",
+             // labelText: "email address",
+            ),
+            onSaved: (val)=>credentials.email=val,
+            validator: (val)=>val==''?val:null,
+          ),
+          TextFormField(
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration.collapsed(
+              hintText: "your password",
+             // labelText: "password",
+            ),
+            onSaved: (val)=>credentials.password=val,
+            validator: (val)=>val==''?val:null,
+          ),
+        ],
+      ),
+    ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize=MediaQuery.of(context).size;
@@ -48,9 +82,52 @@ class _LoginPage extends State<LoginPage> {
     return new Scaffold(
       resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        title: Text('login'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+        title: Text('Login'),
       ),
-      body: new Container(
+      backgroundColor: Colors.teal,
+      body: Stack(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              loadText(),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  border: Border.all(color: Colors.white,width: 1)
+                ),
+                width: MediaQuery.of(context).size.width/2,
+                height: 50,
+                child: new RaisedButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40),),
+                  child: Text('Sign In',style: TextStyle(color: Colors.white),),
+                  color: Colors.teal,
+                  onPressed: signIn,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  border: Border.all(color: Colors.white,width: 1),
+                ),
+                width: MediaQuery.of(context).size.width/2,
+                height: 50,
+                child: new RaisedButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                  color: Colors.teal,
+                  child: Text('Create an account',style: TextStyle(color: Colors.white),),
+                  onPressed: () => Navigator.pushNamed(context, '/createaccount'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+
+      /*
+      new Container(
         width: screenSize.width,
         padding: EdgeInsets.all(20.0),
         //width: screenSize.width,
@@ -76,15 +153,19 @@ class _LoginPage extends State<LoginPage> {
                   validator: (val)=>val==''?val:null,
                 ),
                 new Container(
-                  width: screenSize.width,
+                  width: MediaQuery.of(context).size.width/2,
                   child: new RaisedButton(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
                     child: Text('Sign In'),
+                    color: Colors.teal,
                     onPressed: signIn,
                   ),
                 ),
                 new Container(
-                  width: screenSize.width,
+                  width: MediaQuery.of(context).size.width*2/3,
                   child: new RaisedButton(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                    color: Colors.teal,
                     child: Text('Create an account'),
                     onPressed: () => Navigator.pushNamed(context, '/createaccount'),
                   ),
@@ -93,6 +174,7 @@ class _LoginPage extends State<LoginPage> {
             ),
         ),
       ),
+      */
     );
   }
 }
